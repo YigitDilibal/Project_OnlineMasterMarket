22365ee383e3e2f4b1d128b86ece6bf93ef523fe
YigitDilibal <archery10@outlook.com>
2025-04-30 21:18:46 +0300

    4. Jenkins Test

--- src/test/java/stepdefinitions/GoogleSteps.java
+++ src/test/java/stepdefinitions/GoogleSteps.java
@@ -20,6 +20,7 @@ public class GoogleSteps {
         System.out.println(DataReader.getData("testdata"));
 
         System.out.println("Jenkins");
+        asad
     }
 
     @When("User searches for {string}")

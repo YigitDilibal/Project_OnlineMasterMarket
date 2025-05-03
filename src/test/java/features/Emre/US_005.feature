7f7fdd306528c47ab8fcdc74c7b6d8949f3c33de
emreaatmacaa <yemre.atmaca@icloud.com>
2025-05-04 00:08:21 +0300

    Emre

---
+++ src/test/java/features/Emre/US_005.feature
@@ -0,0 +1,15 @@
+Feature:
+
+  Scenario: Login link should be visible and active in the homepage header section.
+
+    Given the user navigates to the "url" address
+    Then Siteye erişebildiğini doğrular.
+    And Anasayfada Login butonunun gorunur aktif oldugu dogrulanir
+
+  Scenario: LOGIN and (Access to our Online Master Market) texts should be visible in the Login modal.
+
+    Given the user navigates to the "url" address
+    Then Siteye erişebildiğini doğrular.
+    Then Anasayfada Login butonuna tiklar
+
+
--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -5,11 +5,17 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
+import pages.BasePage;
+import pages.HomePage;
 import pages.LoginPage;
+import utils.ReusableMethods;
 
 public class LoginSteps {
 	WebDriver driver = stepdefinitions.Hooks.getDriver();
 	LoginPage loginPage = new LoginPage(driver);
+	BasePage basePage = new BasePage(driver);
+	HomePage homePage = new HomePage(driver);
+
 
 	@Given("the user logs in with username {string} and password {string}")
 	public void the_user_logs_in_with_username_and_password(String email, String pass) {
@@ -18,4 +24,11 @@ public class LoginSteps {
 
 	}
 
+	@Then("Anasayfada Login butonuna tiklar")
+	public void anasayfadaLoginButonunaTiklar() {
+
+		homePage.anasayfaLoginButonu.click();
+		ReusableMethods.bekle(3000);
+
+	}
 }
